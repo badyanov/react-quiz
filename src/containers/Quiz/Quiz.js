0f562519f4363b33685aae46f8b470a1c8068373
Dmitriy <d.badyanov@outlook.com>
2022-05-05 23:44:17 +0300

    Вывод списка ответов

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,7 +4,16 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends Component {
   state = {
-    quiz: [],
+    quiz: [
+      {
+        answers: [
+          { text: "Ответ 1" },
+          { text: "Ответ 2" },
+          { text: "Ответ 3" },
+          { text: "Ответ 4" },
+        ],
+      },
+    ],
   };
 
   render() {
@@ -12,7 +21,7 @@ class Quiz extends Component {
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Quiz!</h1>
-          <ActiveQuiz />
+          <ActiveQuiz answers={this.state.quiz[0].answers} />
         </div>
       </div>
     );
