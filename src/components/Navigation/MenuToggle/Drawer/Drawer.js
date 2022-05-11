a14d3504d43c839090da29ed7c7e45b752fbf4bd
Dmitriy <d.badyanov@outlook.com>
2022-05-11 22:52:29 +0300

    Затемнение основного поля при вызове меню

--- src/components/Navigation/MenuToggle/Drawer/Drawer.js
+++ src/components/Navigation/MenuToggle/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import classes from "./Drawer.module.css";
+import Backdrop from "../../../UI/Backdrop/Backdrop";
 
 const LINKS = [1, 2, 3];
 
@@ -22,9 +23,13 @@ class Drawer extends Component {
     }
 
     return (
-      <nav className={cls.join(" ")}>
-        <ul>{this.renderLinks()}</ul>
-      </nav>
+      <React.Fragment>
+        <nav className={cls.join(" ")}>
+          <ul>{this.renderLinks()}</ul>
+        </nav>
+
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+      </React.Fragment>
     );
   }
 }
